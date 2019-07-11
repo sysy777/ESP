dbc5c4042593404fc9b11b4fe5da38b59a528069
dnri2 <20171989@sungshin.ac.kr>
2019-07-11 13:04:39 +0900

    ㅜㅜ

--- src/Main.java
+++ src/Main.java
@@ -6,30 +6,30 @@ public class Main {
 
 class tools{
     //기존의 음식 삭제 함수
-    void delOld(){
-        for(){
-            System.out.println("String(name of list)");
-        }
-        System.out.println("삭제할 목록 번호를 입력하세요");
-
-        while(true){
-            Scanner in = new Scanner(System.in);
-            if(int i != "yes"){
-                break;
-            }
-        }
-        //들어온 번호 삭제
-        for(){
-            Array.delete(num); //num == 들어온 번호
-        }
-    }
+//    void delOld(){
+//        for(){
+//            System.out.println("String(name of list)");
+//        }
+//        System.out.println("삭제할 목록 번호를 입력하세요");
+//
+//        while(true){
+//            Scanner in = new Scanner(System.in);
+//            if(int i != "yes"){
+//                break;
+//            }
+//        }
+//        //들어온 번호 삭제
+//        for(){
+//            Array.delete(num); //num == 들어온 번호
+//        }
+//    }
 
     //음식 목록 출력 함수 **DB연동
-    void printFood(){
-        for(String : DB){
-            System.out.println("String \n");
-        }
-    }
+//    void printFood(){
+//        for(String : DB){
+//            System.out.println("String \n");
+//        }
+//    }
 
     //새로운 음식 입력
     void inputNew(Scanner in){
