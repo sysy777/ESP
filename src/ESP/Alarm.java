65233e555f030dfa62dadf35a33d73e31d8d9582
smj <seohaa52@daum.net>
2019-08-12 11:48:54 +0900

    alarm class 추가

--- src/ESP/Alarm.java
+++ src/ESP/Alarm.java
@@ -1,5 +1,28 @@
 package ESP;
+import java.io.*;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
 
 public class Alarm {
+    public void alarm(File f){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyMMdd");
+        Calendar c1 = Calendar.getInstance();
+        String strToday = sdf.format(c1.getTime());
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String tuple, date[];
 
+            while ((tuple = br.readLine()) != null){
+                date = tuple.split(" ");
+                if(date[0] == strToday){
+                    System.out.println(date[1]+"의 유통기한이 "+strToday+"까지 입니다.");
+                }
+            }
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
