a43afa51f3c880ebe7aac3b035ab2d9c1b3a7016
smj <seohaa52@daum.net>
2019-08-19 12:53:48 +0900

    CarbonTools class 추가

--- src/ESP/CarbonTools.java
+++ src/ESP/CarbonTools.java
@@ -4,8 +4,9 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class CarbonTools {
-    public static void takeExpired() {
-        File f = new File("expiredFood.txt");
+    public static void takeExpired(File f) {
+        File ef = new File("expiredFood.txt");
+        File cf = new File("carbonConvert.txt");
         try {
             f.createNewFile();
         } catch (IOException e) {
@@ -17,18 +18,31 @@ public class CarbonTools {
         String strToday = sdf.format(c1.getTime());
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader(f));
-            FileWriter fw = new FileWriter(f);
-            String fdline,  carbon, tuple[];
+            BufferedReader fbr = new BufferedReader(new FileReader(f));
+            BufferedReader cbr = new BufferedReader(new FileReader(cf));
+            BufferedWriter efw = new BufferedWriter(new FileWriter(ef,true));
+            PrintWriter epw = new PrintWriter(efw,true);
+            String fdline,  tuple[];
+            int carbon=0;
 
-            while ((fdline = br.readLine()) != null){
+            while ((fdline = fbr.readLine()) != null){
                 tuple = fdline.split(" ");
                 int today=Integer.parseInt(strToday);
                 int fdDate=Integer.parseInt(tuple[1]);
                 if(today - fdDate > 0) {
                     //탄소량 환산(새 변수)
+                    String crbline, ctuple[];
+                    while((crbline = cbr.readLine()) != null){
+                        ctuple = crbline.split(" ");
+                        if(tuple[0].compareTo(ctuple[0])==0){
+                            carbon = Integer.parseInt((ctuple[2]));
+                            break;
+                        }
+                    }
                     //expiredFood에 write tuple 이랑 탄소변수
+                    efw.write(tuple[0] + " "+ tuple[1]+ " " + carbon + "\r\n");
                     //Food에서는 삭제
+
                 }
             }
         }
